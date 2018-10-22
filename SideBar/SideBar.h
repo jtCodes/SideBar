dfea9163231a56c7b2e6884bb1e0a5952557f9b3
jtCodes <jt234wq@gmail.com>
2018-10-22 15:33:14 -0400

    Initial Commit

---
+++ SideBar/SideBar.h
@@ -0,0 +1,19 @@
+//
+//  SideBar.h
+//  SideBar
+//
+//  Created by J Tan on 10/22/18.
+//  Copyright © 2018 J Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideBar.
+FOUNDATION_EXPORT double SideBarVersionNumber;
+
+//! Project version string for SideBar.
+FOUNDATION_EXPORT const unsigned char SideBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideBar/PublicHeader.h>
+
+
